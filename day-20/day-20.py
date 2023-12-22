f87911cc705445abda6cdad1234fc6605ffa6a05
Ramses Kools <ramseskools@hotmail.com>
2023-12-22 12:00:44 +0100

    brute forcing it doesn't work. looking for the pattern does.

--- day-20/day-20.py
+++ day-20/day-20.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from enum import Enum
 from typing import List, Union, Dict
 from queue import Queue
+import os
 
 class BaseModule:
     module_type: str = "base"
@@ -110,6 +111,7 @@ class ModuleConfiguration:
 
     def parse_config_data(self, config_data: List[str]) -> None:
         for line in config_data:
+            line = line.strip()
             module = ModuleFactory.create_module(line)
             self.modules[module.module_name] = module
 
@@ -166,10 +168,16 @@ class PulseManager:
         self.queue = Queue()
         self.low_pulse_count = 0
         self.high_pulse_count = 0
-
+        self.rx_received_low: bool = False
+        self.rx_received_low_button_count: int = 0
         self.pulse_log: List[PulseEvent] = []
+        self.keep_logs: bool = True
+        self.rx_inputs_logs: set[str] = set()
 
     def push_button(self):
+        if not self.rx_received_low:
+            self.rx_received_low_button_count += 1
+
         # Simulate pushing the button by sending a low pulse to the broadcaster
         broadcaster = self.module_config.modules.get("broadcaster")
         button = self.module_config.modules.get("button")
@@ -180,8 +188,13 @@ class PulseManager:
     def process_pulses(self):
         while not self.queue.empty():
             pulse_event = self.queue.get()
-            self.pulse_log.append(pulse_event)
+
+            if self.keep_logs:
+                self.pulse_log.append(pulse_event)
             
+            if self.check_rx_low(pulse_event):
+               self.rx_received_low = True
+
             if pulse_event.pulse_type == PulseType.LOW:
                 self.low_pulse_count += 1
             elif pulse_event.pulse_type == PulseType.HIGH:
@@ -192,14 +205,52 @@ class PulseManager:
             new_pulses = pulse_event.destination.handle_pulse(pulse_event)
             for pulse in new_pulses:
                 self.queue.put(pulse)
+            
+            self.check_rx_inputs_state()
 
     def run_simulation(self, n: int):
         for _ in range(n):
             self.push_button()
             self.process_pulses()
 
+    def run_simulation_until_rx_low(self) -> int:
+        MAX_RUNS = 100000000
+        i = 0
+        while i < MAX_RUNS:
+            if self.rx_received_low:
+                return self.rx_received_low_button_count
+            i += 1
+            self.push_button()
+            self.process_pulses()
+                   
+        return -1        
+
     def get_formatted_pulse_log(self) -> str:
         return "\n".join(str(pulse_event) for pulse_event in self.pulse_log)
+    
+    @staticmethod
+    def check_rx_low(pulse_event: PulseEvent) -> bool:
+        if pulse_event.pulse_type == PulseType.LOW and \
+            pulse_event.destination.module_name == "rx":
+            return True
+        
+        return False
+
+    def check_rx_inputs_state(self):
+        # dr connects to rx
+        # dr == conjunction, so it must remember all high pulses in order to give a low pulse to rx
+        # dr connectors are: qt, qb, ng, mp
+        # let's find a pattern when these send a high pulse ... 
+        dr = self.module_config.modules.get("dr")
+        if not isinstance(dr, ConjunctionModule):
+            return
+                
+        for input, state in dr.states.items():
+            if state == PulseType.HIGH:
+                log = f"Button press: {self.rx_received_low_button_count}, {input} is HIGH"
+                if log not in self.rx_inputs_logs:
+                    print(log)
+                    self.rx_inputs_logs.add(log)
 
 
 def test_example_data() -> None:
@@ -247,7 +298,46 @@ def test_example_data2() -> None:
     pulse_product = pulse_manager.low_pulse_count * pulse_manager.high_pulse_count
     assert pulse_product == 11687500
 
+def readlines_from_file(file_path: str) -> List[str]:
+    assert os.path.exists(file_path)
+
+    lines = []
+    with open(file_path) as f:
+        lines = f.readlines()
+
+    assert len(lines) > 0
+
+    return lines
+
 if __name__ == "__main__":
     test_example_data()
 
     test_example_data2()
+
+    py_file_path = os.path.dirname(__file__)
+    puzzle_doc_path = os.path.join(py_file_path, "puzzle_input.txt")
+
+    puzzle_lines = readlines_from_file(puzzle_doc_path)
+
+    
+    module_config = ModuleConfiguration(puzzle_lines)
+    print("\Puzzle Module Configuration: ")
+    print(module_config)
+
+    pulse_manager = PulseManager(module_config)
+    pulse_manager.run_simulation(1)
+    print("Pulse log of one button push:")
+    print(pulse_manager.get_formatted_pulse_log())
+    pulse_manager.run_simulation(999)
+
+    pulse_product = pulse_manager.low_pulse_count * pulse_manager.high_pulse_count
+    
+    print("\npulse pulse_product:")
+    print(pulse_product)
+
+    pulse_manager.keep_logs = False
+    buttons_presses_until_rx_low = pulse_manager.run_simulation_until_rx_low()
+    
+    print("\nnumber of button presses until rx received low:")
+    print(f"{pulse_manager.rx_received_low_button_count}")
+    print(f"RX received low pulse?: {pulse_manager.rx_received_low}")
